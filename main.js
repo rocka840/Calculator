da4bc0007b136ff3d17a42b246bf30dab2e290ef
unknown <RocKa840@ltpemdomain.local>
2019-12-09 08:08:54 +0100

    hugfda

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
-function clickbutton(x) {
-    alert("You clicked on " + x)
+var cache = 0;
+
+function clickbutton(x) { //alert("You clicked on " + x)
+
+    document.getElementById("result").value += x;
+
+    if (document.getElementById("plusBtn").classList.contains("turnon")) { // Check if button contains the class "turnon"
+        cache = document.getElementById("result") // If true, the button needs to be disabled and is turned off                              
+        document.getElementById("plusBtn").classList.remove("turnon"); // removing the class turns the button off
+    }
 }
-document.getElementById("TheResult")(x);
+
+function reset() {
+    document.getElementById("result").value = "";
+    cache = 0;
+    document.getElementById("result").value = resal;
+}
+
+function plus() {
+    var resal = Number(document.getElementById("result").value);
+    result += Number(cache);
+    document.getElementById("result").value = resal;
+    document.getElementById("plusBtn").classList.add("turnon");
+
+}
